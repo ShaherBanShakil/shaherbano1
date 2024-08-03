91345a9cb1122066d00c86c41f3cf14bab23ac40
ShaherBanShakil <virusshaherbano@gmail.com>
2024-08-03 05:53:58 -0800

    Add files via upload

---
+++ javascript/javascri[pt.js
@@ -0,0 +1,13 @@
+
+var num1=10;
+var num2=60;
+var sum,diff,mul,quo;
+sum=num1+num2;
+diff=num1-num2; 
+mul=num1*num2;  
+quo=num1/num2;
+console.log(sum);
+console.log(diff);
+console.log(mul);
+console.log(quo);
+
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+var num1=10;
+var num2=60;
+let sum,diff,mul,quo;
+sum = num1+num2;
+diff = num1-num2; 
+mul = num1*num2;  
+quo = num1/num2;
+console.log(num1);
+
